f7133578733770a4aea3c63a12b24eada1968069
Administrator <masofeng@126.com>
2020-01-16 10:24:45 +0800

    安装wxminipro组件

---
+++ administrator/components/com_wxminipro/helpers/wxminipro.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package    [PACKAGE_NAME]
+ *
+ * @author     [AUTHOR] <[AUTHOR_EMAIL]>
+ * @copyright  [COPYRIGHT]
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       [AUTHOR_URL]
+ */
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+
+defined('_JEXEC') or die;
+
+/**
+ * WxMinipro helper.
+ *
+ * @package   [PACKAGE_NAME]
+ * @since     1.0.0
+ */
+class WxMiniproHelper
+{
+	/**
+	 * Render submenu.
+	 *
+	 * @param   string  $vName  The name of the current view.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0.0
+	 */
+	public function addSubmenu($vName)
+	{
+		HTMLHelper::_('sidebar.addEntry', Text::_('COM_WXMINIPRO'), 'index.php?option=com_wxminipro&view=wxminipro', $vName === 'wxminipro');
+	}
+}
